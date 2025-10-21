7d7b239fc8d02af4fd4e1a3f1e056c2efef5e11b
dethar <dethar.htun@neozo.de>
2025-10-21 11:03:02 +0700

    feat: add grant roles resource

---
+++ examples/resources/grant_roles/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    oracle = {
+      source = "neozocloud/oracle"
+    }
+  }
+}
+
+provider "oracle" {
+  host     = "localhost"
+  port     = "1521"
+  username = "system"
+  password = "MyPassword123"
+  service  = "orclpdb1"
+}
+
+resource "oracle_grant_roles" "test_grant" {
+  principal  = "testuser"
+  roles = toset([ "connect", "resource" ])
+  grants_mode = "enforce"
+}
